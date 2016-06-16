939f1823aec6616b2d0ce6be757e8f440adc7623
Christopher Bishop <domingobishop@gmail.com>
2016-06-16 17:48:10 +0100

    bg header

--- header.php
+++ header.php
@@ -20,7 +20,7 @@
         <div class="bc-banner">
             <div class="container">
                 <div class="row">
-                    <div class="col-xs-4 col-sm-2 text-center">
+                    <div class="col-md-12 text-center">
                         <a href="<?php bloginfo('siteurl'); ?>/"
                            title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>">
                             <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Heathcote Winery" class="img-responsive">
